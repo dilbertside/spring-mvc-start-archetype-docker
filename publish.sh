66b1cd7b144c04e101c417b5a3e4df2561c19145
dbs <dilbert.side@gmail.com>
2019-01-19 01:54:03 +0100

    bump artefact due to wrong name

--- publish.sh
+++ publish.sh
@@ -6,11 +6,11 @@
 )>&2
 
 
-VERSION_SMSAD=5.1.0
+VERSION_SMSAD=5.1.1
 VERSION_BOM=059b220c4b
 IMG=spring-mvc-start-archetype-docker
 REPO=diside
 
-#docker build --build-arg="VERSION=$VERSION_SMSAD" --build-arg="VERSION_BOM=$VERSION_BOM" --tag="$REPO/$IMG:$VERSION_SMSAD" .
+docker build --build-arg="VERSION=$VERSION_SMSAD" --build-arg="VERSION_BOM=$VERSION_BOM" --tag="$REPO/$IMG:$VERSION_SMSAD" .
 
 docker push $REPO/$IMG:$VERSION_SMSAD
