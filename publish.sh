bcf412957e2f6beaa598ce024dbcf00808f76c46
dbs <2637724+dilbertside@users.noreply.github.com>
2019-04-04 22:48:32 +0200

    upgrade archetype

--- publish.sh
+++ publish.sh
@@ -6,9 +6,9 @@
 )>&2
 
 
-VERSION_SMSAD=5.1.7
-VERSION_SMSA=5.1.7
-VERSION_BOM=5.1.8
+VERSION_SMSAD=5.1.8
+VERSION_SMSA=5.1.8
+VERSION_BOM=5.1.10
 IMG=spring-mvc-start-archetype-docker
 REPO=diside
 
